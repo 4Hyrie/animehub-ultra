34ac2a41150676df9b3462bfe4338bf3214b2e06
4Hyrie <163502950+4Hyrie@users.noreply.github.com>
2026-05-22 16:31:50 +1000

    Rewrite for LampaS 1.12.4 with proper component structure

--- animehub_ultra.js
+++ animehub_ultra.js
@@ -1,17 +1,64 @@
-Lampa.Plugin.register('animehub_ultra', function() {
+// AnimeHub ULTRA - Anime Catalog
+(function () {
+    "use strict";
 
-    Lampa.MainPage.add({
-        name: 'animehub_ultra',
-        title: '🎬 AnimeHub ULTRA',
-        description: 'Anime catalog',
-        icon: '🎬',
-        component: function() {
-            Lampa.Activity.push({
-                title: '🎬 AnimeHub ULTRA',
-                component: 'full',
-                html: '<div style="padding:20px;color:#fff"><h1>Welcome to AnimeHub ULTRA</h1><p>Coming soon...</p></div>'
-            });
+    Lampa.Lang.add({
+        animehub_title: {
+            ru: "AnimeHub ULTRA",
+            en: "AnimeHub ULTRA",
+            uk: "AnimeHub ULTRA",
+            be: "AnimeHub ULTRA",
+            zh: "AnimeHub ULTRA",
+            pt: "AnimeHub ULTRA",
+            bg: "AnimeHub ULTRA"
         }
     });
 
-});
+    function animehub_component(object) {
+        this.create = function () { };
+        this.build = function () { };
+        this.start = function () { };
+        this.pause = function () { };
+        this.stop = function () { };
+        this.render = function () { };
+        this.destroy = function () { };
+    }
+
+    function add() {
+        var ico = '🎬';
+        var menu_items = $(
+            '<li class="menu__item selector" data-action="animehub_ultra"><div class="menu__ico">' +
+            ico +
+            '</div><div class="menu__text">' +
+            Lampa.Lang.translate("animehub_title") +
+            "</div></li>"
+        );
+
+        menu_items.on("hover:enter", function () {
+            if (Lampa.Noty) {
+                Lampa.Noty.show("AnimeHub ULTRA");
+            }
+        });
+
+        $(".menu .menu__list").eq(0).append(menu_items);
+    }
+
+    function createAnimeHub() {
+        window.plugin_animehub_ready = true;
+        Lampa.Component.add("animehub_component", animehub_component);
+        
+        if (window.appready) {
+            add();
+        } else {
+            Lampa.Listener.follow("app", function (e) {
+                if (e.type == "ready") {
+                    add();
+                }
+            });
+        }
+    }
+
+    if (!window.plugin_animehub_ready) {
+        createAnimeHub();
+    }
+})();
